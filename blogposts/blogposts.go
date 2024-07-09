8e717a841140b71b8de440dd421d376f9d85fcd5
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-07-09 23:58:40 +0500

    came to adding Title into Post

---
+++ blogposts/blogposts.go
@@ -0,0 +1,29 @@
+package blogposts
+
+import (
+	"errors"
+	"io/fs"
+)
+
+type Post struct {
+	Title string
+}
+
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, i always fail")
+}
+
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
+	var posts []Post
+	for range dir {
+		posts = append(posts, Post{})
+	}
+	return posts, nil
+}
---
+++ blogposts/blogposts_test.go
@@ -0,0 +1,30 @@
+package blogposts
+
+import (
+	"reflect"
+	"testing"
+	"testing/fstest"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
+	}
+
+	posts, err := NewPostsFromFS(fs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+
+	got := posts[0]
+	want := Post{Title: "Post 1"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
